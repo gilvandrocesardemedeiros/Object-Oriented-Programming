cff889563ad0f7342739f40270bb735f74665474
John Victor <johnvictorio3@gmail.com>
2018-10-22 23:58:38 -0300

    Implementando resolução por algoritmo genético

---
+++ Sudoku/sudoku_genetico.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>     /* srand, rand */
+#include <fstream>
+#include <string>
+#include <stack>
+#include <time.h>
+
+#include "sudoku.h"
+
+#include "sudoku_genetico.h"
+
+sudoku_genetico::sudoku_genetico()
+{
+
+    //preencher aleatoriamente o sudoku
+    //avaliar se as casas geradas são válidas para gerar o fitness
+    //criar lógica para mutação e cruzamento
+
+}
+

1a45376369227d8663444b3c52153e2a8e120fca
Gilvandro César <30601875+gilvandrocesardemedeiros@users.noreply.github.com>
2018-10-24 12:41:59 -0300

    Add files via upload

--- Sudoku/moc_mainsudoku.cpp
+++ Sudoku/moc_mainsudoku.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "mainsudoku.h"
+#include "../../../Downloads/PA/Sudoku_Final/sudoku/mainsudoku.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)

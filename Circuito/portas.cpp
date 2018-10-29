454e3909229a5c10c4c1825d33bbf3753647852b
Gilvandro César <30601875+gilvandrocesardemedeiros@users.noreply.github.com>
2018-10-29 17:12:20 -0300

    Add files via upload

--- Circuito/portas.cpp
+++ Circuito/portas.cpp
@@ -9,7 +9,6 @@ using namespace std;
 Porta::Porta(unsigned NI){
     //Numero de inputs nao pode ser superior ao num_max_inputs
     if(NI > NUM_MAX_INPUTS_PORTA){
-        ///Essa eh uma boa pratica de programacao??
         for(unsigned i = 0; i < NUM_MAX_INPUTS_PORTA; i++) id_in[i] = 0;
         return;
     }
@@ -42,4 +41,86 @@ void Porta::setNumInputs(unsigned N){
     }
 }
 
- void setSaida(bool3S s);
+void Porta::setSaida(bool3S s){
+    saida = s;
+}
+
+void Porta::setId_in(unsigned i, int Id){
+    if(i >= Nin){
+        return;
+    }
+    else{
+        id_in[i] = Id;
+    }
+}
+
+void Porta::digitar(){
+    unsigned N_inputs;
+    do{
+        cout << "Numero de entradas da porta: (2 a 4) ";
+        cin >> N_inputs;
+    }while(N_inputs < 2 || N_inputs > 4);
+    Nin = N_inputs;
+    for(unsigned i = 0; i < Nin; i++){
+        cout<<"id da entrada " <<i + 1<< ": " << endl;
+        cin >> id_in[i];
+    }
+}
+
+bool Porta::ler(istream &I){
+    unsigned N_inputs;
+    I >> ws;
+    I >> N_inputs;
+    if( N_inputs < 2 || N_inputs > 4) return false;
+    Nin = N_inputs;
+    I.ignore(1,':');
+    for(unsigned i = 0; i < Nin; i++){
+        I >> id_in[i];
+        if(id_in[i] == 0) return false;
+    }
+    return true;
+}
+
+ostream &Porta::imprimir(ostream &O) const{
+    O << Nin << ": ";
+    for(unsigned i = 0; i < Nin; i++) O <<' '<<id_in[i];
+    O << "\n";
+    return O;
+}
+
+void Porta_NOT::setNumInputs(unsigned N){
+
+}
+
+
+
+
+/*bool Porta::ler(istream &I){
+    string cabecalho1;
+    I >> cabecalho1;
+    if(cabecalho1 != "CIRCUITO:"){
+        return  false;
+    }
+    unsigned N_inputs, N_outputs, N_portas;
+    I >> N_inputs;
+    if(N_inputs < 2 || N_inputs > 4) return false;
+    I >> N_outputs;
+    I >> N_portas;
+    // Serah que vai precisar? I >> ws;
+    string cabecalho2;
+    I >> cabecalho2;
+    if(cabecalho2 != "PORTAS:") return false;
+    for(unsigned i = 0; i < N_portas; i++){
+        unsigned indice_linha;
+        I >> ws;
+        I >> indice_linha;
+        I.ignore();
+        if(indice_linha != i+1) return  false;
+
+    }
+    Nin = N_inputs;
+    I.ignore(1,':');
+    for(unsigned i = 0 ; i < Nin ; i++)
+      I >> id_in[i];
+}
+*/
